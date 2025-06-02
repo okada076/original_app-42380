d6e01cd9b0cef0ba18d9a27e716467f6dcf7a4db
okada076 <416876pom@gmail.com>
2025-06-02 18:10:27 +0900

    postsコントローラー作成、ルーティング設定

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+
+    if @post.save
+      redirect_to root_path, notice: '投稿が完了しました'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :category, :image, :vegetable_id)
+  end
+end
