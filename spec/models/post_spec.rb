b9a5e8f943a4c84f94023f56b3a56b60a60f16d0
okada076 <416876pom@gmail.com>
2025-06-03 10:51:10 +0900

    Postのモデル単体テストの実装

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
+  RSpec.describe Post, type: :model do
+  before do
+    @post = FactoryBot.build(:post)
+  end
 
-RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '投稿の保存' do
+    context '内容に問題がない場合' do
+      it 'すべての項目が入力されていれば保存できる' do
+        expect(@post).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'タイトルが空では保存できない' do
+        @post.title = ''
+        expect(@post).to_not be_valid
+        expect(@post.errors.full_messages).to include("Title can't be blank")
+      end
+
+      it '内容が空では保存できない' do
+        @post.content = ''
+        expect(@post).to_not be_valid
+        expect(@post.errors.full_messages).to include("Content can't be blank")
+      end
+
+      it 'カテゴリーが空では保存できない' do
+        @post.category = nil
+        expect(@post).to_not be_valid
+        expect(@post.errors.full_messages).to include("Category can't be blank")
+      end
+
+      it '画像が添付されていないと保存できない' do
+        @post.image = nil
+        expect(@post).to_not be_valid
+        expect(@post.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it '野菜が紐づいていないと保存できない' do
+        @post.vegetable = nil
+        expect(@post).to_not be_valid
+        expect(@post.errors.full_messages).to include("Vegetable must exist")
+      end
+
+      it 'ユーザーが紐づいていないと保存できない' do
+        @post.user = nil
+        expect(@post).to_not be_valid
+        expect(@post.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
+end
 end
