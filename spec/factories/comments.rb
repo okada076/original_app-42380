0ada3442ada35ef8dc59cd34b1555b5699ef3552
okada076 <416876pom@gmail.com>
2025-06-04 16:54:59 +0900

    commentのモデル単体テストコード作成

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :comment do
-    content { "MyText" }
-    user { nil }
-    post { nil }
+    content { 'テストコメント' }
+    association :user
+    association :post
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = build(:comment) # FactoryBot を使ってコメントを作成
+  end
+
+  describe 'コメントの保存' do
+    context '内容に問題がない場合' do
+      it 'contentがあれば保存できる' do
+        expect(@comment).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'contentが空では保存できない' do
+        @comment.content = ''
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Content can't be blank")
+      end
+
+      it 'userが紐づいていないと保存できない' do
+        @comment.user = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include('User must exist')
+      end
+
+      it 'postが紐づいていないと保存できない' do
+        @comment.post = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include('Post must exist')
+      end
+    end
+  end
 end
