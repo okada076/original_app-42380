52b8ef537901d53b1fb4790c6b8e2e1c9551bd1e
okada076 <416876pom@gmail.com>
2025-06-05 17:02:46 +0900

    TagsController を作成、ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, only: :create
     resources :likes, only: [:create, :destroy]
+    resources :tags, only: [:show]
   end
 
   # Defines the root path route ("/")
