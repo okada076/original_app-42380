504ba031ce327b3b8819c8ab6236f4a5df882e4d
okada076 <416876pom@gmail.com>
2025-06-05 11:03:51 +0900

    LikesController を作成

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,16 @@
+class LikesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @post = Post.find(params[:post_id])
+    current_user.likes.create(post: @post)
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    like = current_user.likes.find_by(post_id: @post.id)
+    like.destroy if like
+    redirect_to post_path(@post)
+  end
+end
