83467a4953d4433edffe8ad6d84c1aa49db2d709
okada076 <416876pom@gmail.com>
2025-06-06 14:51:20 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,10 @@ Rails.application.routes.draw do
     resources :likes, only: [:create, :destroy]
   end
 
+  resources :vegetables do
+    resources :growing_steps, only: [:index]
+  end
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
