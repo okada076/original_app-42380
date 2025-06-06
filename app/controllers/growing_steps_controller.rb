38c405dcee5e7ae1d4b0f1219c929b2251996708
okada076 <416876pom@gmail.com>
2025-06-06 14:53:49 +0900

    Growing_Steps_Controller設定

--- app/controllers/growing_steps_controller.rb
+++ app/controllers/growing_steps_controller.rb
@@ -1,4 +1,6 @@
 class GrowingStepsController < ApplicationController
   def index
+    @vegetable = Vegetable.find(params[:vegetable_id])
+    @growing_steps = @vegetable.growing_steps.order(:step_number)
   end
 end
