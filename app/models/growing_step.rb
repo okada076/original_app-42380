ef9de206bfb437f51f170f74b104cb6c5c58bf1b
okada076 <416876pom@gmail.com>
2025-06-06 18:01:04 +0900

    アソシエーションを追加

--- app/models/growing_step.rb
+++ app/models/growing_step.rb
@@ -1,5 +1,7 @@
 class GrowingStep < ApplicationRecord
   belongs_to :vegetable
+  has_many :step_progresses
+  has_many :users_checked, through: :step_progresses, source: :user
 
   validates :title, :content, :step_number, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
+  has_many :step_progresses
+  has_many :checked_steps, through: :step_progresses, source: :growing_step
 end
