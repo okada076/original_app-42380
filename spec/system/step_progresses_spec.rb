646364d71adf0bde1e3cad82ef1a9f2cdbc0d12b
okada076 <416876pom@gmail.com>
2025-06-08 13:11:48 +0900

    STEP進捗機能のテスト修正と安定化

--- spec/system/step_progresses_spec.rb
+++ spec/system/step_progresses_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 def sign_in_as(user)
+  Capybara.reset_sessions!
+  visit destroy_user_session_path if defined?(destroy_user_session_path) # Deviseログアウト
   visit new_user_session_path
+  expect(page).to have_field('Email') # 念のため確認
   fill_in 'Email', with: user.email
   fill_in 'Password', with: user.password
   click_button 'Log in'
@@ -10,58 +13,46 @@ end
 RSpec.describe 'StepProgresses', type: :system do
   include Warden::Test::Helpers
 
+  let!(:user) { create(:user) }
+  let!(:vegetable) { create(:vegetable, name: "トマト#{SecureRandom.hex(4)}") }
+  let!(:step1) { create(:growing_step, vegetable: vegetable, step_number: 1, title: '種まき', content: '種をまく') }
+
   before do
     driven_by(:selenium_chrome_headless)
-
-    @user = FactoryBot.create(:user)
-    @vegetable = FactoryBot.create(:vegetable, name: "トマト#{SecureRandom.hex(4)}")
-    @step1 = FactoryBot.create(:growing_step, vegetable: @vegetable, step_number: 1, title: '種まき', content: '種をまく')
-
-    3.times do |i|
-      FactoryBot.create(:growing_step, vegetable: @vegetable, step_number: i + 1, title: "STEP#{i + 1}", content: "内容#{i + 1}")
-    end
+    login_as(user, scope: :user)
   end
 
   it 'STEPをチェックすると、保存されてリロード後もチェックが維持されている' do
-    sign_in_as(@user)
-    visit vegetable_growing_steps_path(@vegetable)
-
-    checkbox_id = "step_checkbox_#{@step1.id}"
-
-    save_and_open_page
+    visit vegetable_growing_steps_path(vegetable)
+    checkbox_id = "step_checkbox_#{step1.id}"
     expect(page).to have_unchecked_field(checkbox_id)
-
     check(checkbox_id)
     visit current_path
     expect(page).to have_checked_field(checkbox_id)
   end
 
   it 'チェックを入れると記録が保存される（別のSTEP表示名）' do
-    sign_in_as(@user)
-    visit vegetable_growing_steps_path(@vegetable)
-
-    checkbox_id = "step_checkbox_#{@step1.id}"
-
+    Capybara.reset_sessions!
+    sign_in_as(user) # ← CapybaraでUIログイン
+    visit vegetable_growing_steps_path(vegetable)
+    checkbox_id = "step_checkbox_#{step1.id}"
     expect(page).to have_unchecked_field(id: checkbox_id)
     check(checkbox_id)
-
     visit current_path
     expect(page).to have_checked_field(id: checkbox_id)
   end
 
   it 'ログインユーザーが手順ページへ遷移できる' do
-    sign_in_as(@user)
-
+    ogin_as(user, scope: :user)
     visit root_path
     click_link '作り方ガイド'
-
     expect(current_path).to eq(vegetables_path)
 
-    # 修正ポイント（リンクが見つからない問題に対応）
-    find("a[href='#{vegetable_growing_steps_path(@vegetable)}']").click
+    # リンクを正確に指定
+    find("a[href='#{vegetable_growing_steps_path(vegetable)}']").click
 
-    expect(current_path).to eq(vegetable_growing_steps_path(@vegetable))
-    expect(page).to have_content("#{@vegetable.name} の作り方ガイド")
+    expect(current_path).to eq(vegetable_growing_steps_path(vegetable))
+    expect(page).to have_content("#{vegetable.name} の作り方ガイド")
   end
 
   it '未ログインユーザーは育て方手順ページにアクセスできずログインページにリダイレクトされる' do
