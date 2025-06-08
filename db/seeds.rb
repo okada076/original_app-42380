c620c40e90b4a3506e4ee546c746c56b57a84fb1
okada076 <416876pom@gmail.com>
2025-06-08 14:43:02 +0900

    デバッグ用にトマトのデータ確認を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts Vegetable.find_by(name: "トマト").inspect
 
 [
   "トマト", "きゅうり", "にんじん", "レモン", "さつまいも",
