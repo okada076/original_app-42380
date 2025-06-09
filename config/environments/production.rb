b1291f6d246651f075336bfb8b37ae6e1e2a4518
okada076 <416876pom@gmail.com>
2025-06-09 19:49:03 +0900

    本番環境でActiveStorage画像のURLが生成されない不具合を修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,4 +101,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  Rails.application.routes.default_url_options[:host] = 'https://original-app-42380.onrender.com'
+  
 end
