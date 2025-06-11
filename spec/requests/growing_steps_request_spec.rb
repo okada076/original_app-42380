d26cc0e32095b44f8f56df4449246bd21b3ee4fd
okada076 <416876pom@gmail.com>
2025-06-11 12:18:59 +0900

    indexのリクエストスペックを修正

--- spec/requests/growing_steps_request_spec.rb
+++ spec/requests/growing_steps_request_spec.rb
@@ -10,9 +10,12 @@ RSpec.describe "GrowingSteps", type: :request do
       sign_in user
     end
 
-    it "returns http success" do
-      get "/growing_steps/index"
-      expect(response).to have_http_status(:success)
-    end
+    let(:vegetable) { FactoryBot.create(:vegetable) }
+
+it "returns http success" do
+  sign_in user
+  get vegetable_growing_steps_path(vegetable)
+  expect(response).to have_http_status(:success)
   end
+ end
 end
