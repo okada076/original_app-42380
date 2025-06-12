59b210b230e3a7a24807842acf3d1ae684b99255
okada076 <416876pom@gmail.com>
2025-06-12 12:06:23 +0900

    seeds_controller.rbを修正

--- app/controllers/admin/seeds_controller.rb
+++ app/controllers/admin/seeds_controller.rb
@@ -1,6 +1,5 @@
 class Admin::SeedsController < ApplicationController
-  lass SeedsController < ApplicationController
-    # 誰でも実行できてしまうので、必要なら簡単な認証をつけても良い
+  
     skip_before_action :verify_authenticity_token
 
     def run
