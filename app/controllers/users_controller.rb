2f004baece688536a8a2706fa612dd5a64c6b62f
okada076 <416876pom@gmail.com>
2025-07-04 18:15:47 +0900

    liked_postsアクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def liked_posts
+    @user = current_user
+    @liked_posts = @user.liked_posts.includes(:vegetable, :user)
+  end
 end
