042dda37d7b0d4cede94e451ab278d867abe1bf7
okada076 <416876pom@gmail.com>
2025-07-04 18:44:48 +0900

    image_processingインストール

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,6 @@ class UsersController < ApplicationController
 
   def liked_posts
     @user = current_user
-    @liked_posts = @user.liked_posts.includes(:vegetable, :user)
+    @liked_posts = @user.liked_posts.includes(:user, :vegetable, image_attachment: :blob)
   end
 end
