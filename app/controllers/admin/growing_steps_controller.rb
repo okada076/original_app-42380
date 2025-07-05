8056c079e9ff098a13a0ad0735aa962291baa618
okada076 <416876pom@gmail.com>
2025-07-05 23:35:16 +0900

    管理者制限の追加

--- app/controllers/admin/growing_steps_controller.rb
+++ app/controllers/admin/growing_steps_controller.rb
@@ -1,7 +1,31 @@
 class Admin::GrowingStepsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_admin!
+
   def new
+    @growing_step = GrowingStep.new
+    @vegetables = Vegetable.all
   end
 
   def create
+     @growing_step = GrowingStep.new(growing_step_params)
+    if @growing_step.save
+      redirect_to new_admin_growing_step_path, notice: "育て方手順を追加しました！"
+    else
+      @vegetables = Vegetable.all
+      render :new
+    end
+  end
+
+  private
+
+  def growing_step_params
+    params.require(:growing_step).permit(:vegetable_id, :step_number, :title, :content)
+  end
+
+  def ensure_admin!
+    unless current_user.email == "あなたのメールアドレス"
+      redirect_to root_path, alert: "アクセス権限がありません"
+    end
   end
 end
